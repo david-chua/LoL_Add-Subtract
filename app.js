664b175c24e2bce66aacded9d4e42388925d2d73
dp_chua <dp_chua@hotmail.com>
2017-04-15 03:11:49 -0400

    created stickers for play

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("javascript is working")
+
+var level;
+
+// Clock Countdown
+var timer;
+var minutes;
+var seconds;
+var interval;
+
+randomizes stickers on board
+var annie = "http://i.imgur.com/44DRP87.png";
+var braum = "http://i.imgur.com/bkoQzSh.png";
+var jinx = "http://i.imgur.com/J7ybNrV.png";
+var vi = "http://i.imgur.com/BgL0ZGD.png";
+var zed = "http://i.imgur.com/120qWrR.png";
+var objs = [annie, braum, jinx, vi, zed];
+var test= [];
